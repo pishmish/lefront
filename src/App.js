6fdc2dcf00d40fdf654cc030285c1afc5d9121f1
Guanghui Ma <guang@Huis-MacBook-Pro.local>
2024-10-26 17:02:54 +0300

    hello changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState} from 'react'
 
-
+//hello
 function App() {
 
   const [backendData, setBackendData] = useState([{}]) //i dont really understand this but it sets the backend data
